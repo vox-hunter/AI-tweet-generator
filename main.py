f64bdcb61df9475db68c045116f464c5e56f9e99
⎝⎝ＶＯＸ ⎠⎠ <vidyuts@gardenbangkok.com>
2024-06-21 19:18:42 +0700

    improved ui

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from fetch import fetch
 import streamlit as st
 
 st.title("Tweet Generator")
-topic = st.text_input("Enter the topic of the tweet")
-mood = st.text_input("Enter the mood of the tweet")
-style = st.text_input("Enter the style of the tweet")
-if st.button("Generate Tweet"):
+
+with st.form("tweet_form"):
+    topic = st.text_input("Enter the topic of the tweet")
+    mood = st.text_input("Enter the mood of the tweet")
+    style = st.text_input("Enter the style of the tweet")
+    generate_button = st.form_submit_button("Generate Tweet")
+
+if generate_button:
     tweet = fetch(topic, mood, style)
     st.write(tweet)
