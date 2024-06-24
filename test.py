ef6c6afb2e540fc9e296032686a14795a31ef820
⎝⎝ＶＯＸ ⎠⎠ <vidyuts@gardenbangkok.com>
2024-06-24 19:03:15 +0700

    beta

---
+++ test.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from streamlit_oauth import OAuth2Component
+import os
+import base64
+import json
+
+# import logging
+# logging.basicConfig(level=logging.INFO)
+
+# create an OAuth2Component instance
+CLIENT_ID = "541107227366-es3sgd8on1fgivcv3mu5rm5ocdbbr884.apps.googleusercontent.com"
+CLIENT_SECRET = "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"
+AUTHORIZE_ENDPOINT = "https://accounts.google.com/o/oauth2/v2/auth"
+TOKEN_ENDPOINT = "https://oauth2.googleapis.com/token"
+REVOKE_ENDPOINT = "https://oauth2.googleapis.com/revoke"
+
+if "auth" not in st.session_state:
+    # create a button to start the OAuth2 flow
+    oauth2 = OAuth2Component(CLIENT_ID, CLIENT_SECRET, AUTHORIZE_ENDPOINT, TOKEN_ENDPOINT, TOKEN_ENDPOINT, REVOKE_ENDPOINT)
+    result = oauth2.authorize_button(
+        name="Continue with Google",
+        icon="https://www.google.com.tw/favicon.ico",
+        redirect_uri="http://localhost:8501",
+        scope="openid email profile",
+        key="google",
+        extras_params={"prompt": "consent", "access_type": "offline"},
+        use_container_width=True,
+        pkce='S256',
+    )
+    
+    if result:
+        st.write(result)
+        # decode the id_token jwt and get the user's email address
+        id_token = result["token"]["id_token"]
+        # verify the signature is an optional step for security
+        payload = id_token.split(".")[1]
+        # add padding to the payload if needed
+        payload += "=" * (-len(payload) % 4)
+        payload = json.loads(base64.b64decode(payload))
+        email = payload["email"]
+        st.session_state["auth"] = email
+        st.session_state["token"] = result["token"]
+        st.rerun()
+else:
+    st.write("You are logged in!")
+    st.write(st.session_state["auth"])
+    st.write(st.session_state["token"])
+    if st.button("Logout"):
+        del st.session_state["auth"]
+        del st.session_state["token"]
+        st.rerun()
